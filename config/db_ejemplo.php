73bec7b1a8ccd6bb001a1a9eacce9246adeaaf7c
Ricardo Peña García <rickypega48@gmail.com>
2026-04-10 16:37:23 -0400

    conexion para la db (probando host)

---
+++ config/db_ejemplo.php
@@ -0,0 +1,32 @@
+<?php
+// Archivo de configuración para la conexión a la base de datos
+//copien exactamente este código y peguenlo en un nuevo archivo llamado db.php dentro de la carpeta config
+class Database {
+    // Configuración para XAMPP (Local)
+    private $host = "localhost";
+    private $db_name = "Docuhuella";
+    private $username = "root";
+    private $password = ""; 
+    public $conn;
+
+    // Método para obtener la conexión
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            // Habilitar errores para desarrollo
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
