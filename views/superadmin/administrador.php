e7d41f74fe6bcebc587ea43687b8e392be103e7b
Ricardo Peña García <rickypega48@gmail.com>
2026-04-14 13:42:51 -0400

    Nombre de titulos

--- views/superadmin/administrador.php
+++ views/superadmin/administrador.php
@@ -39,7 +39,7 @@ $admins = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Clínicas - DocuHuella</title>
+    <title>DocuHuella</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
